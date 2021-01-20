76c4eaf5ee4e9d776235bab6060d30dd2bb2e0ae
ZagoskinVA <zagoskin.3@mail.ru>
2021-01-20 11:32:40 +0300

    cpplint

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,7 @@
 #include "postfix.h"
 
 
-std::string inf("(2 + 8.3) * (6 - 3.2)");
-std::string postf = infix2postfix(inf);
-
 int main() {
-std::cout << postf << std::endl;
+std::cout << infix2postfix("(2 + 8.3) * (6 - 3.2)") << std::endl;
 return 0;
 }
