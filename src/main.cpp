8ed9746d55baff1343dc8ae7b07df7779ff9b460
ZagoskinVA <zagoskin.3@mail.ru>
2021-01-20 10:49:23 +0300

    Fixed cpplint

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,12 @@
 
 
 int main() {
- std::string inf = "(2 + 8.3) * (6 - 3.2)";
- std::string postf = infix2postfix(inf);
- std::cout << postf << std::endl;
- inf = "2 + 6 * 3 / (4 - 2)";
- std::cout << infix2postfix(inf) << std::endl;
- inf = "(6 * (4 - 2) + 5) * (2.6 + 3 * 7) - 9";
- std::cout << infix2postfix(inf) << std::endl;
- return 0;
+std::string inf = "(2 + 8.3) * (6 - 3.2)";
+std::string postf = infix2postfix(inf);
+std::cout << postf << std::endl;
+inf = "2 + 6 * 3 / (4 - 2)";
+std::cout << infix2postfix(inf) << std::endl;
+inf = "(6 * (4 - 2) + 5) * (2.6 + 3 * 7) - 9";
+std::cout << infix2postfix(inf) << std::endl;
+return 0;
 }
--- src/postfix.cpp
+++ src/postfix.cpp
@@ -3,71 +3,69 @@
 #include "postfix.h"
 #include "MyStack.h"
 
-std::string infix2postfix(std::string inf)
-{
- MyStack<char> operators(400);
- std::string post = "";
- char sumb = '\0';
- for (int i = 0; i < inf.length(); i++) 
- {
-  if (inf[i] == '(')
-   operators.push(inf[i]);
-  else if (inf[i] == ')') {
-   while (!operators.isEmpty()) {
-    sumb = operators.pop();
-    if (sumb == '(')
-     break;
-    post += sumb;
-   }
-  }
-  else if (inf[i] == '+' || inf[i] == '-') {
-   while (!operators.isEmpty()) {
-    if (operators.get() == '(') {
-     break;
-    }
-    post += operators.pop();
-   }
-   operators.push(inf[i]);
-  }
-  else if (inf[i] == '*' || inf[i] == '/') {
-   while (!operators.isEmpty()) {
-    char sb = operators.get();
-    if (sb == '+' || sb == '-' || sb == '(') {
-     break;
-    }
-    post += operators.pop();
-   }
-   operators.push(inf[i]);
-  }
-  else {
-   post += inf[i];
-  }
- }
- while (!operators.isEmpty()) {
-  post += operators.pop();
- }
- std::string result = "";
- result += post[0];
- for (int i = 1; i < post.length(); i++) {
-  if (post[i] == ' ' && post[i - 1] == ' ' &&
-   post[i + 1] > 47 && post[i + 1] < 58) {
-   continue;
-  }
-  else if ((post[i] == '+' || post[i] == '-' || post[i] == '/' || post[i] == '*') &&
-   (post[i + 1] == '+' || post[i + 1] == '-' 
-    || post[i + 1] == '/' || post[i + 1] == '*')) {
-   result += post[i];
-   result += ' ';
-  }
-  else if (post[i] > 47 && post[i] < 58 &&
-   (post[i + 1] == '+' || post[i + 1] == '-' 
-    || post[i + 1] == '/' || post[i + 1] == '*')) {
-   result += post[i];
-   result += ' ';
-  }
-  else{
-   result += post[i];
-   }
- }
- return result;
+std::string infix2postfix(std::string inf) {
+MyStack<char> operators(400);
+std::string post = "";
+char sumb = '\0';
+for (int i = 0; i < inf.length(); i++) {
+if (inf[i] == '(')
+operators.push(inf[i]);
+else if (inf[i] == ')') {
+while (!operators.isEmpty()) {
+sumb = operators.pop();
+if (sumb == '(')
+break;
+post += sumb;
+}
+}
+else if (inf[i] == '+' || inf[i] == '-') {
+while (!operators.isEmpty()) {
+if (operators.get() == '(') {
+break;
+}
+post += operators.pop();
+}
+operators.push(inf[i]);
+}
+else if (inf[i] == '*' || inf[i] == '/') {
+while (!operators.isEmpty()) {
+char sb = operators.get();
+if (sb == '+' || sb == '-' || sb == '(') {
+break;
+}
+post += operators.pop();
+}
+operators.push(inf[i]);
+}
+else {
+post += inf[i];
+}
+}
+while (!operators.isEmpty()) {
+post += operators.pop();
+}
+std::string result = "";
+result += post[0];
+for (int i = 1; i < post.length(); i++) {
+if (post[i] == ' ' && post[i - 1] == ' ' &&
+post[i + 1] > 47 && post[i + 1] < 58) {
+continue;
+}
+else if ((post[i] == '+' || post[i] == '-' || post[i] == '/' || post[i] == '*') &&
+(post[i + 1] == '+' || post[i + 1] == '-' 
+|| post[i + 1] == '/' || post[i + 1] == '*')) {
+result += post[i];
+result += ' ';
+}
+else if (post[i] > 47 && post[i] < 58 &&
+(post[i + 1] == '+' || post[i + 1] == '-' 
+|| post[i + 1] == '/' || post[i + 1] == '*')) {
+result += post[i];
+result += ' ';
+}
+else{
+result += post[i];
+
+}
+return result;
 }
