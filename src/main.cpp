3284e9273439628bee94ce3d64ae3f4f48081f85
ZagoskinVA <zagoskin.3@mail.ru>
2021-01-20 10:58:42 +0300

    Fixed cpplint

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,12 @@
 
 
 int main() {
-std::string inf = "(2 + 8.3) * (6 - 3.2)";
+std::string inf ("(2 + 8.3) * (6 - 3.2)");
 std::string postf = infix2postfix(inf);
 std::cout << postf << std::endl;
-inf = "2 + 6 * 3 / (4 - 2)";
-std::cout << infix2postfix(inf) << std::endl;
-inf = "(6 * (4 - 2) + 5) * (2.6 + 3 * 7) - 9";
-std::cout << infix2postfix(inf) << std::endl;
+std::string inf1 ("2 + 6 * 3 / (4 - 2)");
+std::cout << infix2postfix(inf1) << std::endl;
+std::string inf2 ("(6 * (4 - 2) + 5) * (2.6 + 3 * 7) - 9");
+std::cout << infix2postfix(inf2) << std::endl;
 return 0;
 }
